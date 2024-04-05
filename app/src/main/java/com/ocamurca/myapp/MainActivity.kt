55e38b667884629e0c5ac4e8139c9ae030668058
Gabriel Camurça <gabriel@brtech.dev>
2024-04-05 12:01:50 -0300

    Implements calculate logic.

--- app/src/main/java/com/ocamurca/myapp/MainActivity.kt
+++ app/src/main/java/com/ocamurca/myapp/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.ocamurca.myapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ocamurca.myapp.ui.theme.MyappTheme
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var mEditTextEstado: EditText
+    private lateinit var mEditTextValor: EditText
+    private lateinit var mTextViewPorcentagem: TextView
+    private lateinit var mTextViewTotal: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mEditTextEstado = findViewById(R.id.editTextEstado)
+        mEditTextValor = findViewById(R.id.editTextValor)
+        mTextViewPorcentagem = findViewById(R.id.textViewPorcentagem)
+        mTextViewTotal = findViewById(R.id.textViewTotal)
+
+        val buttonCalcular = findViewById<Button>(R.id.button)
+        buttonCalcular.setOnClickListener {
+            calcular(it)
+        }
+    }
+
+    private fun calcular(view: View) {
+        val estado = mEditTextEstado.text.toString()
+        val valorString = mEditTextValor.text.toString()
+
+        val valor = valorString.toFloat()
+        val porcentagem: Double
+
+        porcentagem = when (estado) {
+            "RO" -> {
+                17.5
+            }
+            "SP" -> {
+                18.0
+            }
+            else -> {
+                1.0
+            }
+        }
+
+        val total = valor + (valor * porcentagem / 100)
+        val formattedPercentage = String.format(getString(R.string.porcentagem_text), porcentagem)
+        mTextViewPorcentagem.text = formattedPercentage
+        val formattedTotal = String.format(getString(R.string.total_text), total)
+        mTextViewTotal.text = formattedTotal
     }
 }
